108e305af60fc646d0c0fc18e1a0c53a4b618c60
joloujo <joloujo28@gmail.com>
2021-06-11 18:07:24 -0700

    Player Controller V2
    Better turning, change how input are detected and how they affect the movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,28 +15,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontal_torque = 1;
-    public float vertical_force = 1;
+    public float thrust = 1;
+    public float spin_speed = 1;
 
     public Vector2 max_speed = new Vector2(10, 10);
     public float max_angle = 45;
     public bool enforce_max = true;
 
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
+
+
     private Rigidbody2D rb;
+    private int Direction;
 
     void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update() 
+    {
+        if (Input.GetKeyDown(Left))
+        {
+            Direction = -1;
+        }
+        else if (Input.GetKey(Left) && !Input.GetKey(Right))
+        {
+            Direction = -1;
+        }
+        else if (Input.GetKeyDown(Right))
+        {
+            Direction = 1;
+        }
+        else if (Input.GetKey(Right) && !Input.GetKey(Left))
+        {
+            Direction = 1;
+        }
+        else if (Input.GetKey(Left) == false && Input.GetKey(Right) == false)
+        {
+            Direction = 0;
+        }
+    }
+
     void FixedUpdate()
     {
-        rb.AddForce((Input.GetAxisRaw("Vertical") * vertical_force) * transform.up);
-        rb.AddTorque(Input.GetAxisRaw("Horizontal") * -horizontal_torque);
-
+        rb.AddForce((Input.GetAxisRaw("Vertical") * thrust) * transform.up);
+        //rb.rotation += Input.GetAxisRaw("Horizontal") * -spin_speed;
+        rb.rotation += Direction * -spin_speed;
         
         if (enforce_max) {
-            /*if (rb.velocity[0] >= max_speed[0]) {
+
+            if (rb.velocity[0] >= max_speed[0]) {
                 rb.velocity = new Vector2 (max_speed[0], rb.velocity[1]);
             } else if (rb.velocity[0] <= -max_speed[0]) {
                 rb.velocity = new Vector2 (-max_speed[0], rb.velocity[1]);
@@ -46,13 +76,14 @@ public class PlayerController : MonoBehaviour
                 rb.velocity = new Vector2 (rb.velocity[0], max_speed[1]);
             } else if (rb.velocity[1] <= -max_speed[1]) {
                 rb.velocity = new Vector2 (rb.velocity[0], -max_speed[1]);
-            }*/
+            }
 
             if (rb.rotation >= max_angle) {
                 rb.rotation = max_angle;
             } else if (rb.rotation <= -max_angle) {
                 rb.rotation = -max_angle;
             }
+
         }
     }
 }
